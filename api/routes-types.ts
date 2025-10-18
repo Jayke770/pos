9613581eda8095890b12d0ab7af4c6fae1e26307
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2025-10-18 22:27:00 +0800

    .

--- api/routes-types.ts
+++ api/routes-types.ts
@@ -4,7 +4,7 @@ import type Route1 from "./routes/categories";
 import type Route2 from "./routes/auth";
 
 declare global {
-	export type Routes = ElysiaWithBaseUrl<"/api/inventory", typeof Route0> &
-		ElysiaWithBaseUrl<"/api/categories", typeof Route1> &
-		ElysiaWithBaseUrl<"/api/auth", typeof Route2>;
+    export type Routes = ElysiaWithBaseUrl<"/api/inventory", typeof Route0>
+              & ElysiaWithBaseUrl<"/api/categories", typeof Route1>
+              & ElysiaWithBaseUrl<"/api/auth", typeof Route2>
 }
