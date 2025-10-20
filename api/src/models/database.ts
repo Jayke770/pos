734135cf007059a295984408bd125f78af852e97
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2025-10-20 15:26:28 +0800

    .

--- api/src/models/database.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import { envConfig } from "@/lib/environment";
-
-const client = postgres(envConfig.DATABASE_URL);
-const db = drizzle({ client });
-export { db };
--- src/app/dashboard/inventory/page.tsx
+++
@@ -1,4 +0,0 @@
-import InventoryPage from "@/app/dashboard/inventory/main";
-export default async function Page() {
-	return <InventoryPage />;
-}
