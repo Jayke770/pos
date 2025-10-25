9d6dc2f68889c339264653496988d0bf2b0659ab
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2025-10-25 20:18:11 +0800

    feat: add server:start script and improve loading component
    - Added a new script "server:start" to package.json for running the backend app.
    - Removed unnecessary devDependencies related to Vite from package.json.
    - Updated the Loading component to accept an optional className prop for better customization.
    - Created a new Loader component in src/app/loading.tsx that utilizes the Loading component.

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,13 +1,14 @@
+import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { motion } from "motion/react";
 
-export default function Loading() {
+export default function Loading({ className }: { className?: string }) {
     return (
         <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
-            className=" w-full h-full flex justify-center items-center flex-1">
+            className={cn(" w-full h-full flex justify-center items-center flex-1", className)}>
             <Loader2 className=" size-12 animate-spin text-primary" />
         </motion.div>
     );
